82eed1504f6922ed6ae9c89b2b0ce8df92bfdcec
mdabagia <maxdabagia@gmail.com>
2022-06-10 21:11:18 -0400

    updated brain.py
    adjusted the BrainArea class to allow for multiple sources of input, as
    well as adding/changing some existing methods

--- brain.py
+++ brain.py
@@ -17,9 +17,9 @@ class BrainArea():
     
     
     Attributes
-    ----------
-    n_inputs : int
-        number of inputs
+    ----------    
+    n_inputs : array of shape (n_input_areas)
+        number of inputs (per input area)
         
     n_neurons : int
         number of neurons
@@ -36,8 +36,11 @@ class BrainArea():
     normalize : bool
         whether to initialize weights as normalized
         
-    input_weights : array of shape (n_inputs, n_neurons)
-    	weights between brain area and input neurons
+    n_input_areas : int
+        number of input areas
+        
+    input_weights : array of shape (n_input_areas, n_inputs, n_neurons)
+    	weights between brain area and input areas
     	
     recurrent_weights : array of shape (n_neurons, n_neurons)
     	weights between brain area neurons
@@ -50,7 +53,7 @@ class BrainArea():
         
     Methods
     -------
-    project(input=None)
+    set_input(inputs, input_area=0)
         Loads external input to the brain area
         
     step(update=True)
@@ -59,19 +62,26 @@ class BrainArea():
     read()
         Returns current activations
         
-    reset()
+    inhibit()
         Sets current activations to zero
+        
+    fire(activations)
+        Manually fires neurons to match the given pattern
+        
         
     normalize()
         Normalizes current incoming weights of each neuron to unit sum
+        
+    reset()
+        Resets weights, activations, and inputs
     '''
     
     def __init__(self, n_inputs, n_neurons, cap_size, density, plasticity=1e-1, normalize=False):
         '''
         Parameters
         ----------
-        n_inputs : int
-            number of inputs
+        n_inputs : int or array
+            number of inputs per area
 
         n_neurons : int
             number of neurons
@@ -88,63 +98,100 @@ class BrainArea():
         normalize : bool, optional
             whether to initialize weights as normalized (default is False)
         '''
-        
+        if isinstance(n_inputs, int):
+            self.n_input_areas = 1
+            n_inputs = [n_inputs]
+        else:
+            self.n_input_areas = len(n_inputs)
         self.n_inputs = n_inputs
         self.n_neurons = n_neurons
         self.cap_size = cap_size
         self.density = density
         self.plasticity = plasticity
-        self.input_weights = (rng.random((n_inputs, n_neurons)) < density) * 1.
-        self.recurrent_weights = (rng.random((n_neurons, n_neurons)) < density) * 1.
-        if normalize:
-            self.normalize()
-        self.inputs = np.zeros(n_inputs)
-        self.activations = np.zeros(n_neurons)
+        self.reset()
         
-    def project(self, inputs):
+    def set_input(self, inputs, input_area=0):
         '''Loads input to the brain area.
         
         Parameters
         ----------
-        inputs : ndarray of shape (n_neurons)
+        inputs : array of shape (n_inputs[input_area]) or array of arrays
             The input to the brain area on the current time step
+            
+        input_area : int or array, optional
+            The areas to input (default is zero)
         '''
-
-        self.inputs = inputs
+        if len(inputs) != self.n_inputs[input_area]:
+            raise Exception('Mismatched dimensions in input and specified area {} ({} not equal to {})'.format(input_area, len(inputs), self.n_inputs[input_area]))
+        if isinstance(input_area, int):
+            self.inputs[input_area] = inputs
+        else:
+            for i in input_area:
+                self.inputs[i] = inputs[i]
         
     def step(self, update=True):
         '''Updates activations to next time step.
         
-        Also resets current input to zero after applying it.
+        Also resets current inputs to zero after applying it.
         
         Parameters
         ----------
         update : bool, optional
             Whether to update the weights based on the resulting activations (default is True)
         '''
-        new_activations = k_cap(self.inputs @ self.input_weights + self.activations @ self.recurrent_weights, self.cap_size)
+        new_activations = k_cap(np.sum([self.inputs[i] @ self.input_weights[i] for i in range(self.n_input_areas)], axis=0) + self.activations @ self.recurrent_weights, self.cap_size)
         
         if update:
-            self.input_weights[(self.inputs[:, np.newaxis] > 0) & (new_activations[np.newaxis] > 0)] *= 1 + self.plasticity
+            for w, inp in zip(self.input_weights, self.inputs):
+                w[(inp[:, np.newaxis] > 0) & (new_activations[np.newaxis] > 0)] *= 1 + self.plasticity
             self.recurrent_weights[(self.activations[:, np.newaxis] > 0) & (new_activations[np.newaxis] > 0)] *= 1 + self.plasticity
 
         self.activations = new_activations
-        self.inputs = np.zeros(self.n_inputs)
+        self.inputs = [np.zeros(n) for n in self.n_inputs]
         
     def read(self):
         '''Returns current activations.
         '''
-        return self.activations
+        return self.activations.copy()
     
-    def reset(self):
+    def fire(self, activations, update=True):
+        '''Manually fires neurons to match the given pattern.
+        
+        Parameters
+        ----------
+        activations : array of shape (n_neurons)
+            The firing pattern to match
+        
+        update : bool, optional
+            Whether to update the weights based on the resulting activations (default is True)
+        '''
+        if update:
+            for w, inp in zip(self.input_weights, self.inputs):
+                w[(inp[:, np.newaxis] > 0) & (activations[np.newaxis] > 0)] *= 1 + self.plasticity
+            self.recurrent_weights[(self.activations[:, np.newaxis] > 0) & (activations[np.newaxis] > 0)] *= 1 + self.plasticity
+        self.activations = activations
+        self.inputs = [np.zeros(n) for n in self.n_inputs]
+        
+
+    def inhibit(self):
         '''Sets current activations and inputs to zero.
         '''
-        self.inputs = np.zeros(self.n_inputs)
+        self.inputs = [np.zeros(n) for n in self.n_inputs]
         self.activations = np.zeros(self.n_neurons)
     
     def normalize(self):
         '''Normalizes current incoming weights of each neuron to unit sum.
         '''
-        norm = self.input_weights.sum(axis=0) + self.recurrent_weights.sum(axis=0)
-        self.input_weights /= norm[np.newaxis, :]
+        norm = np.sum([w.sum(axis=0) for w in self.input_weights], axis=0) + self.recurrent_weights.sum(axis=0)
+        for w in self.input_weights:
+            w /= norm[np.newaxis, :]
         self.recurrent_weights /= norm[np.newaxis, :]
+        
+    def reset(self):
+        '''Resets weights, activations, and inputs.
+        '''
+        self.input_weights = [(rng.random((n, self.n_neurons)) < self.density) * 1. for n in self.n_inputs]
+        self.recurrent_weights = (rng.random((self.n_neurons, self.n_neurons)) < self.density) * 1.
+        self.inhibit()
+        if self.normalize:
+            self.normalize()
