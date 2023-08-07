78d5a729f7e9c3ce74c4932a52815291e72367ac
mdabagia <maxdabagia@gmail.com>
2023-08-07 15:17:04 -0400

    Updated demo

--- brain.py
+++ brain.py
@@ -1,284 +1,234 @@
 import numpy as np
-import matplotlib.pyplot as plt
 rng = np.random.default_rng()
 
 def k_cap(input, cap_size):
-    output = np.zeros_like(input)
-    if np.any(input != output):
-        if len(input.shape) == 1:
-            idx = np.argsort(input)[-cap_size:]
-            output[idx] = 1
-        else:
-            idx = np.argsort(input, axis=-1)[..., -cap_size:]
-            np.put_along_axis(output, idx, 1, axis=-1)
-    return output
+    if np.all(input == 0):
+        return []
+    else:
+        return input.argsort(axis=-1)[...,-cap_size:]
 
-class BrainArea():
-    '''A class representing a single brain area and its input.
-    
-    
-    Attributes
-    ----------    
-    n_inputs : array of shape (n_input_areas)
-        number of inputs (per input area)
-        
-    n_neurons : int
-        number of neurons
-        
-    cap_size : int
-        number of neurons allowed to fire simultaneously
-        
-    density : float
-        probability of random connection between neurons
-        
-    plasticity : float
-        strength of the Hebbian update
-        
-    norm_init : bool
-        whether to initialize weights as normalized
-        
-    n_input_areas : int
-        number of input areas
-        
-    input_weights : array of shape (n_input_areas, n_inputs, n_neurons)
-    	weights between brain area and input areas
-    	
-    recurrent_weights : array of shape (n_neurons, n_neurons)
-    	weights between brain area neurons
-    	
-    inputs : array of shape (n_inputs)
-    	current inputs to the brain area
-    	
-    activations : array of shape (ref_period, n_neurons)
-    	firing patterns in the brain area over the last ref_period rounds, with activations[0] the most recent
-        
-    Methods
-    -------
-    set_input(inputs, input_area=0)
-        Loads external input to the brain area
-        
-    step(update=True)
-        Updates activations to next time step
-        
-    read()
-        Returns current activations
-        
-    inhibit()
-        Sets current activations to zero
-        
-    fire(activations)
-        Manually fires neurons to match the given pattern
-        
-        
-    normalize()
-        Normalizes current incoming weights of each neuron to unit sum
-        
-    reset()
-        Resets weights, activations, and inputs
-    '''
-    
-    def __init__(self, n_inputs, n_neurons, cap_size, density, plasticity=1e-1, norm_init=False, refraction=False, ref_period=2, ref_penalty=1e2):
-        '''
-        Parameters
-        ----------
-        n_inputs : int or array
-            number of inputs per area
+def idx_to_vec(idx, shape):
+    vec = np.zeros(idx.shape[:-1] + (shape,))
+    np.put_along_axis(vec, idx, 1, axis=-1)
+    return vec
 
-        n_neurons : int
-            number of neurons
-
-        cap_size : int
-            number of neurons allowed to fire simultaneously
-
-        density : float
-            probability of random connection between neurons
-
-        plasticity : float, optional
-            strength of the Hebbian update (default is 0.1)
-
-        norm_init : bool, optional
-            whether to initialize weights as normalized (default is False)
-            
-        refraction : bool, optional
-            whether to penalize neurons for firing too many times consecutively (default is False)
-            
-        ref_period : int, optional
-            the number of rounds a neuron may fire consecutively before being penalized (default is 2)
-            
-        ref_penalty : float, optional
-            the refractory penalty (default is 100)
-        '''
+class FFArea():
+    def __init__(self, n_inputs, n_neurons, cap_size, density, plasticity, norm_init=False):
         if isinstance(n_inputs, int):
             self.n_input_areas = 1
             n_inputs = [n_inputs]
         else:
             self.n_input_areas = len(n_inputs)
+            
         self.n_inputs = n_inputs
         self.n_neurons = n_neurons
         self.cap_size = cap_size
         self.density = density
         self.plasticity = plasticity
         self.norm_init = norm_init
-        self.refraction = refraction
-        self.ref_period = ref_period
-        self.penalty = ref_penalty
-        self.bias = np.zeros(n_neurons)
         self.reset()
         
-    def set_input(self, inputs, input_area=0):
-        '''Loads input to the brain area.
-        
-        Parameters
-        ----------
-        inputs : array of shape (n_inputs[input_area]) or array of arrays
-            The input to the brain area on the current time step
-            
-        input_area : int or array, optional
-            The areas to input (default is zero)
-        '''
-        if isinstance(input_area, int):
-            self.inputs[input_area] = inputs
-        else:
-            for i in input_area:
-                self.inputs[i] = inputs[i]
-        
-    def step(self, update=True):
-        '''Updates activations to next time step.
-        
-        Also resets current inputs to zero after applying it.
-        
-        Parameters
-        ----------
-        update : bool, optional
-            Whether to update the weights based on the resulting activations (default is True)
-        '''
-        if self.refraction:
-            self.bias -= self.penalty * np.all(self.activations[:-1] > 0, axis=0)
-
-        self.activations[1:] = self.activations[:-1]
-        self.activations[0] = k_cap(np.sum([self.weight_factor[i] * self.inputs[i] @ self.input_weights[i] for i in range(self.n_input_areas)], axis=0) + self.activations[1] @ self.recurrent_weights + self.bias, self.cap_size)
-
-        if update:
-            for w, inp in zip(self.input_weights, self.inputs):
-                w[(inp[:, np.newaxis] > 0) & (self.activations[0][np.newaxis] > 0)] *= 1 + self.plasticity
-            self.recurrent_weights[(self.activations[1][:, np.newaxis] > 0) & (self.activations[0][np.newaxis] > 0)] *= 1 + self.plasticity
-        self.inputs = [np.zeros(n) for n in self.n_inputs]
-        
-    def read(self):
-        '''Returns current activations.
-        '''
-        return self.activations[0].copy()
-    
-    def fire(self, activations, update=True):
-        '''Manually fires neurons to match the given pattern.
-        
-        Parameters
-        ----------
-        activations : array of shape (n_neurons)
-            The firing pattern to match
-        
-        update : bool, optional
-            Whether to update the weights based on the resulting activations (default is True)
-        '''
-        if update:
-            for w, inp in zip(self.input_weights, self.inputs):
-                w[(inp[:, np.newaxis] > 0) & (activations[np.newaxis] > 0)] *= 1 + self.plasticity
-            self.recurrent_weights[(self.activations[0][:, np.newaxis] > 0) & (activations[np.newaxis] > 0)] *= 1 + self.plasticity
-        self.activations[1:] = self.activations[:-1]
-        self.activations[0] = activations
-        self.inputs = [np.zeros(n) for n in self.n_inputs]
-        
-
-    def inhibit(self):
-        '''Sets current activations and inputs to zero.
-        '''
-        self.inputs = [np.zeros(n) for n in self.n_inputs]
-        self.activations = np.zeros((self.ref_period+1, self.n_neurons))
-        self.bias = np.zeros(self.n_neurons)
-    
-    def normalize(self):
-        '''Normalizes current incoming weights of each neuron to unit sum.
-        '''
-        norm = np.sum([w.sum(axis=0) for w in self.input_weights], axis=0) + self.recurrent_weights.sum(axis=0)
-        for w in self.input_weights:
-            w /= norm[np.newaxis, :]
-        self.recurrent_weights /= norm[np.newaxis, :]
-        
     def reset(self):
-        '''Resets weights, activations, and inputs.
-        '''
         self.input_weights = [(rng.random((n, self.n_neurons)) < self.density) * 1. for n in self.n_inputs]
-        self.recurrent_weights = (rng.random((self.n_neurons, self.n_neurons)) < self.density) * 1.
         self.inhibit()
-        self.weight_input(1.)
         if self.norm_init:
             self.normalize()
-            
-    def project(self, inputs, n_rounds=5, input_area=0, update=True):
-        if len(inputs) > 1:
-            n_rounds = len(inputs)
         
-        for i in range(n_rounds):
-            self.set_input(inputs[0 if len(inputs) == 1 else i], input_area=input_area)
-            self.step(update=update)
+    def inhibit(self):
+        self.clear_input()
+        self.activations = []
+        
+    def fire(self, activations, update=True):
+        if update:
+            self.update(activations)
             
-    def weight_input(self, factor, input_area=None):
-        if input_area is None:
-            self.weight_factor = [factor for _ in self.n_inputs]
-        else:
-            self.weight_factor[input_area] = factor
-                        
-class ScaffoldNetwork():
-    def __init__(self, n_areas, n_inputs, n_neurons, cap_size, density, plasticity=1e-1, norm_init=False):
-        self.n_areas = n_areas
-        self.areas = [BrainArea(n_inputs + [n_neurons], n_neurons, cap_size, density, plasticity=plasticity, norm_init=norm_init) for _ in range(n_areas)]
+        self.activations = activations
+        self.clear_input()
         
     def set_input(self, inputs, input_area=0):
-        for area in self.areas:
-            area.set_input(inputs, input_area=input_area)
-                    
+        if isinstance(input_area, int):
+            if len(inputs) == self.n_input_areas:
+                self.inputs = inputs
+            else:
+                self.inputs[input_area] = inputs
+        else:                    
+            for i in input_area:
+                self.inputs[i] = inputs[i]
+                
+    def clear_input(self, input_area=-1):
+        if isinstance(input_area, int):
+            if input_area < 0:
+                self.inputs = [[] for _ in self.n_inputs]
+            else:
+                self.inputs[input_area] = []
+        else:
+            for i in input_area:
+                self.inputs[i] = []
+                
+    def get_total_input(self):
+        return np.sum([w[inp].sum(axis=0) for w, inp in zip(self.input_weights, self.inputs)], axis=0)
+                
     def step(self, update=True):
-        for i in range(self.n_areas):
-            self.areas[i].set_input(self.areas[i-1].read(), input_area=-1)
-        for area in self.areas:
-            area.step(update=update)
+        new_activations = k_cap(self.get_total_input(), cap_size=self.cap_size)
+        if update:
+            self.update(new_activations)
             
-    def fire(self, activations, areas=None, update=True):
-        if areas is None:
-            areas = [i for i in range(self.n_areas)]
-        elif isinstance(areas, int):
-            areas = [areas]
-            activations = [activations]
-
-        for i in range(self.n_areas):
-            self.areas[i].set_input(self.areas[i-1].read(), input_area=-1)
+        self.activations = new_activations
+        self.clear_input()
         
-        for i, act in zip(areas, activations):
-            self.areas[i].fire(act, update=update)    
+    def forward(self, inputs, input_area=0, update=True):
+        self.set_input(inputs, input_area=input_area)
+        self.step(update=update)
+        self.clear_input()
+        
+                
+    def update(self, new_activations):
+        for w, inp in zip(self.input_weights, self.inputs):
+            w[np.ix_(inp, new_activations)] *= 1 + self.plasticity
+    
+    def normalize(self):
+        for w, inp in zip(self.input_weights, self.inputs):
+            w /= w.sum(axis=0, keepdims=True)
+        
+    def read(self, dense=False):
+        if dense:
+            return idx_to_vec(self.activations, self.n_neurons)
+        else:
+            return self.activations
+        
+class RecurrentArea(FFArea):
+    def __init__(self, n_inputs, n_neurons, cap_size, density, plasticity, norm_init=False):
+        super().__init__(n_inputs, n_neurons, cap_size, density, plasticity, norm_init)
+        
+    def reset(self):
+        self.recurrent_weights = (rng.random((self.n_neurons, self.n_neurons)) < self.density) * 1.
+        super().reset()                
+        
+    def update(self, new_activations):
+        super().update(new_activations)
+        self.recurrent_weights[np.ix_(self.activations, new_activations)] *= 1 + self.plasticity
+        
+    def get_total_input(self):
+        return super().get_total_input() + self.recurrent_weights[self.activations].sum(axis=0)
+        
+    def normalize(self):
+        super().normalize()
+        self.recurrent_weights /= self.recurrent_weights.sum(axis=0, keepdims=True)
+        
+class RefractedArea(FFArea):
+    def __init__(self, n_inputs, n_neurons, cap_size, density, plasticity, norm_init=False):
+        super().__init__(n_inputs, n_neurons, cap_size, density, plasticity, norm_init)
+        
+    def reset(self):
+        self.bias = np.zeros(self.n_neurons)
+        super().reset()
+        
+    def update(self, new_activations):
+        self.bias[new_activations] += super().get_total_input()[new_activations] * self.plasticity
+        super().update(new_activations)
+        
+    def get_total_input(self):
+        return super().get_total_input() - self.bias
+        
+class RandomChoiceArea(RecurrentArea):
+    def __init__(self, n_neurons, cap_size, density, plasticity, norm_init=False):
+        super().__init__([], n_neurons, cap_size, density, 3., norm_init)
+        
+    def train(self, assemblies):
+        for assm in assemblies:
+            self.inhibit()
+            self.fire(assm)
+            self.fire(assm)
+        self.inhibit()
+        
+    def flip(self, n_rounds=10):
+        self.inhibit()
+        self.fire(rng.choice(self.n_neurons, size=self.cap_size, replace=False), update=False)
+        for _ in range(n_rounds):
+            self.step(update=False)
+        choice = self.read()
+        self.inhibit()
+        return choice
+        
+class ScaffoldNetwork():
+    def __init__(self, n_inputs, n_neurons, cap_size, density, plasticity, norm_init=False):
+        self.areas = [RecurrentArea([n_inputs, n_neurons], n_neurons, cap_size, density, plasticity, norm_init=norm_init),
+                      RecurrentArea(n_neurons, n_neurons, cap_size, density, plasticity, norm_init=norm_init)]
+        
+    def reset(self):
+        for area in self.areas:
+            area.reset()
             
-        for i in range(self.n_areas):
-            if i not in areas:
-                self.areas[i].step(update=update)
-
-    def read(self):
-        return [area.read() for area in self.areas]
-                    
     def inhibit(self):
         for area in self.areas:
             area.inhibit()
-    
-    def reset(self):
+            
+    def set_input(self, inputs):
+        self.areas[0].set_input(inputs)
+        
+    def step(self, update=True):
+        self.areas[0].set_input(self.areas[1].read(), input_area=1)
+        self.areas[1].set_input(self.areas[0].read())
         for area in self.areas:
-            area.reset()       
-             
+            area.step(update=update)
+            
+    def forward(self, inputs, update=True):
+        self.set_input(inputs)
+        self.step(update=update)
+    
     def normalize(self):
         for area in self.areas:
             area.normalize()
             
-    def weight_interarea(self, factor):
-        self.weight_input(factor, input_area=-1)
-
-    def weight_input(self, factor, input_area=None):
-        for area in self.areas:
-            area.weight_input(factor, input_area=input_area)
-
+    def read(self, dense=False):
+        return self.areas[0].read(dense=dense)
+            
+class FSMNetwork():
+    def __init__(self, n_symbol_neurons, n_state_neurons, n_arc_neurons, cap_size, density, plasticity, norm_init=False):
+        self.state_area = FFArea(n_arc_neurons, n_state_neurons, cap_size, density, plasticity, norm_init=norm_init)
+        self.arc_area = RefractedArea([n_symbol_neurons, n_state_neurons], n_arc_neurons, cap_size, density, plasticity, norm_init=norm_init)
+        
+    def reset(self):
+        self.state_area.reset()
+        self.arc_area.reset()
+        
+    def inhibit(self):
+        self.state_area.inhibit()
+        self.arc_area.inhibit()
+        
+    def forward(self, inputs, update=True):
+        self.arc_area.forward([inputs, self.state_area.read()], update=update)
+        self.state_area.forward(self.arc_area.read(), update=update)
+        
+    def read(self, dense=False):
+        return self.state_area.read(dense=dense)
+        
+    def train(self, symbol, state, new_state):
+        self.inhibit()
+        self.arc_area.forward([symbol, state])
+        self.state_area.set_input(self.arc_area.read())
+        self.state_area.fire(new_state)
+        
+class PFANetwork():
+    def __init__(self, n_symbol_neurons, n_state_neurons, n_arc_neurons, n_random_neurons, cap_size, density, plasticity, norm_init=False):
+        self.symbol_area = FFArea(n_arc_neurons, n_symbol_neurons, cap_size, density, plasticity, norm_init=norm_init)
+        self.state_area = FFArea(n_arc_neurons, n_state_neurons, cap_size, density, plasticity, norm_init=norm_init)
+        self.arc_area = RefractedArea([n_state_neurons, n_random_neurons], n_arc_neurons, cap_size, density, plasticity, norm_init=norm_init)
+        self.random_area = RandomChoiceArea(n_random_neurons, cap_size, density, plasticity)
+        self.cap_size = cap_size
+        self.random_area.train([np.arange(cap_size), np.arange(cap_size, 2 * cap_size)])
+        
+    def train(self, state, rand, new_state, symbol):
+        self.arc_area.forward([state, np.arange(rand*self.cap_size, (rand+1)*self.cap_size)])
+        self.symbol_area.set_input(self.arc_area.read())
+        self.symbol_area.fire(symbol)
+        self.state_area.set_input(self.arc_area.read(), input_area=0)
+        self.state_area.fire(new_state)
+        
+    def step(self):
+        self.arc_area.forward([self.state_area.read(), self.random_area.flip()], update=False)
+        self.state_area.forward(self.arc_area.read(), update=False)
+        self.symbol_area.forward(self.arc_area.read(), update=False)
+        
+    def read(self, dense=False):
+        return self.symbol_area.read(dense=dense)
